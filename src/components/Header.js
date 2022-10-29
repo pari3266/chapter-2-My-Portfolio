820848e02c9ee4a381a3ce320deff0d774d4f4ca
pari3266 <parisatalebi25@gmail.com>
2022-10-29 18:30:46 +0330

    test

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header({ logo }) {
   return (
     <header className='App-header'>
       <img src={logo} className='App-logo' alt='logo' />
-      <h1>My Portfolio</h1>
+      <h1>*My Portfolio*</h1>
       <nav>
         <RouterLink to='/' className='App-link'>
           About me
