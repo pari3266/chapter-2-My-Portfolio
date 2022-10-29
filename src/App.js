6f5196f9a96a0bd2ead2a264fec0c159fdb937e1
pari3266 <parisatalebi25@gmail.com>
2022-10-29 18:26:35 +0330

    My Portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Profile from './pages/Profile';
+import Projects from './pages/Projects';
+import ProjectDetail from './pages/ProjectDetail';
+import logo from './assets/logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <BrowserRouter>
+        <Header logo={logo} />
+        <Routes>
+          <Route path='/' element={<Profile userName='pari3266' />} />
+          <Route path='/projects' element={<Projects userName='pari3266' />} />
+          <Route
+            path='/projects/:name'
+            element={<ProjectDetail userName='pari3266' />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
